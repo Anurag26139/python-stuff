3bc6b0ffbdf1ba8f937ebd89b4a57a320bdec4db
Anurag Bajpai <anuragbajpai2802@gmail.com>
2022-07-21 23:42:52 +0530

    Array DSA

---
+++ array dsa.py
@@ -0,0 +1,7 @@
+n=['2200','2350','2600','2130','2190']
+print("Dollars Extra spend in compare to jan:",n)
+print("Total expense in first quarter:",n[0]+n[1]+n[2])
+print("Did we expeand 200 in n:",200 in n)
+n.append(1980)
+print("Corrected Data is :",n)
+print("Final list is :",n)
---
+++ first element to appear k times.py
@@ -0,0 +1,6 @@
+l=[1,7,4,3,4,7,8]
+n=len(l)
+print(l.count(7))
+for i in n:
+    
+
