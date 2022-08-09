8bdf4381e4ed3e502420dfb3bbe6c2bfc25a35a9
Anurag Bajpai <anuragbajpai2802@gmail.com>
2022-08-09 23:58:19 +0530

    remove duplicate link list

---
+++ remove duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]
+        temp=head
+        while temp:
+            while temp.next and temp.next.next==temp.val
+                temp.next==temp.next.next
+            temp=temp.next
+        return head
