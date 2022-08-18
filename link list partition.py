565fbfba767fc6cbe8a1d214128b8f34695628e9
Anurag Bajpai <anuragbajpai2802@gmail.com>
2022-08-18 22:32:24 +0530

    partition

---
+++ link list partition.py
@@ -0,0 +1,16 @@
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        d1=c1=ListNode(0)
+        d2=c2=ListNode(0)
+        while head:
+            if head.val<x:
+                c1.next=head
+                c1=c1.next
+            else:
+                c2.next=head
+                c2=c2.next
+            head=head.next
+        c2.next=None
+        c1.next=d2.next
+        return d1.next
+        
